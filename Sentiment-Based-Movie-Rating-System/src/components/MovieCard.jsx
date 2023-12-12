2e56a0c13fcc31263bcb496091ea605cf93b4448
SameeraDayaratne <85645373+SameeraDayaratne@users.noreply.github.com>
2023-12-12 15:11:14 +0530

    Implemented Movie Card

---
+++ Sentiment-Based-Movie-Rating-System/src/components/MovieCard.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+
+function MovieCard({ imgSrc, ...props }) {
+  return (
+    <div>
+      <div
+        {...props}
+        className="relative max-w-[180px] max-h-[260px] overflow-hidden  shadow-lg group"
+      >
+        <img
+          src={imgSrc}
+          alt=""
+          className="transition-transform group-hover:scale-110 duration-200"
+        />
+        <div className="absolute inset-0 flex items-end text-xs gap-2 bg-gradient-to-t pb-2 pl-2 from-black/60 to-transparent">
+          <div className="py-2/3 px-2 rounded-lg text-white text-[10px] bg-[#ff5100]">
+            Action
+          </div>
+          <div className="py-2/3 px-2 rounded-lg text-white text-[10px] bg-[#ff5100]">
+            Sci Fi
+          </div>
+        </div>
+      </div>
+      <div className=" mt-2">
+        <h2 className="text-white">Venom</h2>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
