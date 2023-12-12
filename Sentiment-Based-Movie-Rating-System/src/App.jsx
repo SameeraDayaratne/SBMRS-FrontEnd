ccef07cc80d886aee89031dadbedeb1f522ddf23
SameeraDayaratne <85645373+SameeraDayaratne@users.noreply.github.com>
2023-12-12 16:19:20 +0530

    Implemented Movie Card Skeleton

--- Sentiment-Based-Movie-Rating-System/src/App.jsx
+++ Sentiment-Based-Movie-Rating-System/src/App.jsx
@@ -1,23 +1,22 @@
 /* eslint-disable no-unused-vars */
 
-import './App.css'
-import Hero from './components/Hero'
-import HeroNew from './components/HeroNew'
-import Navbar from './components/Navbar'
-import Main from './components/Main'
+import "./App.css";
+import Hero from "./components/Hero";
+import HeroNew from "./components/HeroNew";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import { SkeletonTheme } from "react-loading-skeleton";
 
 function App() {
-
-
   return (
-   <div className='font-poppins'>
-    <Navbar />
-    <Hero />
-    <Main />
-   </div>
-   
-    
-  )
+    <SkeletonTheme baseColor="#202020" highlightColor="#444">
+      <div className="font-poppins">
+        <Navbar />
+        <Hero />
+        <Main />
+      </div>
+    </SkeletonTheme>
+  );
 }
 
-export default App
+export default App;
--- Sentiment-Based-Movie-Rating-System/src/components/Main.jsx
+++ Sentiment-Based-Movie-Rating-System/src/components/Main.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 import ex from "../assets/ex.jpg";
 import a from "../assets/1.jpg";
@@ -8,16 +8,33 @@ import d from "../assets/3.jpg";
 import f from "../assets/4.jpg";
 import g from "../assets/5.jpg";
 import h from "../assets/6.jpg";
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import MovieCardSkeleton from "./MovieCardSkeleton";
+
 
 function Main(props) {
+
+    const [isLoading , setIsLoading] = useState(true)
+
+    useEffect(() => {
+        setTimeout(()=> {
+            setIsLoading(false);
+        },3000);
+    } , [])
+    
   return (
     <div className="mt-14 max-w-5xl mx-auto">
         <div>
             <h2 className="text-white font-bold text-xl">Most Popular</h2>
             <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-[#ff5100]"></hr>
         </div>
-      <div className="grid md:grid-cols-5 gap-8 items-center place-items-center justify-center my-8 ">
-        <MovieCard imgSrc={ex} />
+      
+        {isLoading && <div className="grid md:grid-cols-5 gap-8 items-center place-items-center justify-center my-8">
+            <MovieCardSkeleton cards={15}/>
+        </div> }
+        {!isLoading && <div className="grid md:grid-cols-5 gap-8 items-center place-items-center justify-center my-8">
+            <MovieCard imgSrc={ex} />
         <MovieCard imgSrc={a} />
         <MovieCard imgSrc={s} />
         <MovieCard imgSrc={d} />
@@ -37,7 +54,10 @@ function Main(props) {
         <MovieCard imgSrc={a} />
         <MovieCard imgSrc={s} />
         <MovieCard imgSrc={d} />
-      </div>
+        </div>
+        
+}
+      
     </div>
   );
 }
